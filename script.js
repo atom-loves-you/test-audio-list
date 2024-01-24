68e5fa0f7839442f38a7dbf92e4047bf6ce3d3d3
Khristin <atom.loves.you.2022@gmail.com>
2024-01-23 17:02:55 -0800

    Metadata updated
    Each track has been re-uploaded to update metadata

--- script.js
+++ script.js
@@ -1,121 +1,133 @@
-let theme = window.matchMedia("(prefers-color-scheme: dark)").matches ?
-	"dark" :
-	"light";
+let theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "dark"
+  : "light";
 if (theme == "dark") {
-	document.documentElement.classList.add("dark");
+  document.documentElement.classList.add("dark");
 }
 document
-	.getElementById("dark-mode-toggle")
-	.addEventListener("click", function () {
-		document.documentElement.classList.toggle("dark");
-	});
+  .getElementById("dark-mode-toggle")
+  .addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark");
+  });
 document.getElementById("song-saved").addEventListener("click", function () {
-	document.getElementById("song-saved").classList.toggle("saved");
+  document.getElementById("song-saved").classList.toggle("saved");
 });
-Amplitude.init(
-	{
-		bindings:
-		{
-			37: "prev",
-			39: "next",
-			32: "play_pause"
-		},
-		callbacks:
-		{
-			timeupdate: function () {
-				let percentage = Amplitude.getSongPlayedPercentage();
-				if (isNaN(percentage)) {
-					percentage = 0;
-				}
-				/**
-				 * Massive Help from: https://nikitahl.com/style-range-input-css
-				 */
-				let slider = document.getElementById("song-percentage-played");
-				slider.style.backgroundSize = percentage + "% 100%";
-			}
-		},
-		songs: [
+Amplitude.init({
+  bindings: {
+    37: "prev",
+    39: "next",
+    32: "play_pause",
+  },
+  callbacks: {
+    timeupdate: function () {
+      let percentage = Amplitude.getSongPlayedPercentage();
+      if (isNaN(percentage)) {
+        percentage = 0;
+      }
 
-			// Tweeters, Woofers, Heavy Metal
-			{
-				name: "Track 01: Tweeters, Woofers, Heavy Metal",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/wav/Tweeters-Woofers-Heavy Metal.wav",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			},
+      /* Massive Help from: https://nikitahl.com/style-range-input-css */
 
-			{
-				name: "Track 02: The Summer of George",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/wav/Summer of George-FINAL-Spring-2024.wav",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			},
-			{
-				name: "Track 03: Otherkin",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/mp3/Otherkin.mp3",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			},
-			{
-				name: "Track 04: Title",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 05: ℏ = h/2π",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/mp3/ℏ = h_2π.mp3",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 06: Just Don't Write Bugs",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/mp3/Just-Don't-Write-Bugs.mp3",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 07: Ad Astra",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/wav/Ad Astra - THE GOOD ONE.wav",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 00: L'appel du Vide (The Call of the Void)",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/mp3/L'appel du Vide.mp3",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 00: Lo fi",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/wav/Lo fi .wav",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 00: Title: Ooo",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/wav/Ooo.wav",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 00: My Song 36",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/wav/My-Song-36-Updated-on-Dec-31-2023.wav",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			}, {
-				name: "Track 00: Last Track",
-				artist: "Atom Loves You",
-				album: "Ad Astra per Aspera",
-				url: "./audio-files-final/wav/Last track.wav",
-				cover_art_url: "https://assets.codepen.io/6566924/warp.png"
-			},
-		]
-	});
+      let slider = document.getElementById("song-percentage-played");
+      slider.style.backgroundSize = percentage + "% 100%";
+    },
+  },
+
+  songs: [
+    /* Track 01: Tweeters, Woofers, Heavy Metal */
+    {
+      name: "Track 01: Tweeters, Woofers, Heavy Metal",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./audio-files-final/wav/Tweeters-Woofers-Heavy Metal.wav",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 02: The Summer of George */
+    {
+      name: "Track 02: The Summer of George",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./audio/The-Summer-of-George.mp3",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 03: Otherkin*/
+    {
+      name: "Track 03: Otherkin",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 04: ℏ = h/2π */
+    {
+      name: "Track 04: ℏ = h/2π",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./audio/ℏ = h_2π.mp3",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 05: */
+    {
+      name: "Track 05: Just Don't Write Bugs",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 06: */
+    {
+      name: "Track 06: Ad Astra",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./audio/Ad-Astra.mp3",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 07: */
+    {
+      name: "Track 07: Title Here",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 08: */
+    {
+      name: "Track 08: Title Here",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 09: */
+    {
+      name: "Track 09: Title Here",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    },
+
+    /* Track 10: */
+    {
+      name: "Track 10: Title Here",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera",
+      url: "./",
+      cover_art_url: "https://assets.codepen.io/6566924/warp.png",
+    }
+
+    /* Track 11... */
+
+  ],
+});
+
 window.onkeydown = function (e) {
-	return !(e.keyCode == 32);
+  return !(e.keyCode == 32);
 };
